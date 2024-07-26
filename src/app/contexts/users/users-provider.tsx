f5698820312360d13f573d943a8bcaeb2c4187f5
Matheus Pinheiro <matheus.rj.pinheiro@gmail.com>
2024-07-26 08:15:06 -0300

    chore: Add users context and provider

---
+++ src/app/contexts/users/users-provider.tsx
@@ -0,0 +1,65 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios, {endpoints} from "src/utils/axios";
+import { UsersContext } from "./users-context";
+
+interface ProviderProps {
+  children: React.ReactNode;
+}
+
+export function UsersProvider({ children }: ProviderProps){
+  const queryClient = useQueryClient();
+
+  const {data, isLoading, isError} = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axios.get(endpoints.users.list);
+      return res.data;
+    }
+  })
+
+  const { mutateAsync: registerUser } = useMutation({
+    mutationFn: async (formData: any) => {
+      const res = await axios.post(endpoints.users.register, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['users']})
+    },
+  })
+
+  const { mutateAsync: updateUser } = useMutation({
+    mutationFn: async ({id, formData}:{id: string, formData:any}) => {
+      const res = await axios.put(`${endpoints.users.update}${id}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      return res.data;
+      },
+      onSuccess: (data, variables) => {
+        const queryKeys = [
+          ['users'],
+          ['user', variables.id]
+        ]
+        queryKeys.forEach(key => queryClient.invalidateQueries({queryKey: key}))
+      },
+  })
+  
+
+  const { mutateAsync: deleteUser } = useMutation({
+    mutationFn: async(id: string)=> {
+      const res = await axios.delete(`${endpoints.users.delete}/${id}`);
+      return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['users']})
+    }
+
+  })
+
+  return <UsersContext.Provider value={{data, isLoading, isError, registerUser, deleteUser, updateUser}}>{children}</UsersContext.Provider>
+}
